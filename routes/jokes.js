6abdab761271f96f0bdfef98e7c0f10c2c9ce752
Jan Willem Henckel <farly.de@gmail.com>
2022-02-24 12:45:10 +0100

    Update jokes.js

--- routes/jokes.js
+++ routes/jokes.js
@@ -5,19 +5,19 @@ const router = express.Router();
 
 let jokes = [
   {
-    id: 0,
+    id: "fui9Zxj2yXPy8agOBrCvn",
     joke: "Thanks for explaining the word “many” to me, it means a lot.",
   },
   {
-    id: 1,
+    id: "UOwDSulXnp2qtwEPpPFHd",
     joke: "Why did Adele cross the road? To say hello from the other side.",
   },
   {
-    id: 2,
+    id: "VQRSRuszkQaLBZBpZoLWp",
     joke: "What kind of concert only costs 45 cents? A 50 Cent concert featuring Nickelback.",
   },
   {
-    id: 3,
+    id: "nvhGGI9AeEZKyVPDZorjQ",
     joke: "To the person who invented zero, thanks for nothing.",
   },
 ];
